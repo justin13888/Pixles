3d802cf393887a30436b9111a18e57584ba0a28b
Justin Chung <20733699+justin13888@users.noreply.github.com>
2025-02-07 19:50:54 -0500

    pixles-api: Add dumy endpoint

--- pixles-api/graphql/src/schema/user/queries.rs
+++ pixles-api/graphql/src/schema/user/queries.rs
@@ -1,12 +1,23 @@
 use super::types::User;
 use async_graphql::*;
-
+use chrono::Utc;
 pub struct UserQuery;
 
 #[Object]
 impl UserQuery {
     async fn get_user(&self, ctx: &Context<'_>, id: ID) -> Result<User> {
-        todo!()
+        // TODO: Implement
+        Ok(User {
+            id: "123".to_string(),
+            username: "testuser".to_string(),
+            name: "Test User".to_string(),
+            email: "test@test.com".to_string(),
+            account_verified: true,
+            needs_onboarding: false,
+            created_at: Utc::now(),
+            modified_at: Utc::now(),
+            deleted_at: None,
+        })
     }
 
     async fn list_users(&self, ctx: &Context<'_>) -> Result<Vec<User>> {
