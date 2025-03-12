5141b1f4d0e92d644a480a328b3b1459c3196396
Justin Chung <20733699+justin13888@users.noreply.github.com>
2025-03-11 23:14:22 -0400

    pixles-app: Remove a comment

--- pixles-app/codegen.ts
+++ pixles-app/codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-	schema: "http://localhost:3000/graphql", // TODO: Double check this
+	schema: "http://localhost:3000/graphql",
 	documents: ["src/**/*.tsx"],
 	ignoreNoDocuments: true, // for better experience with the watcher
 	generates: {
