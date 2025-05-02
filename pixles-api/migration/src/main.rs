76a661186e4d101364e7115c07b8fbed9ab5201f
Justin Chung <20733699+justin13888@users.noreply.github.com>
2025-05-01 20:36:16 -0400

    pixles-api: Fix migration error

--- pixles-api/migration/src/main.rs
+++ pixles-api/migration/src/main.rs
@@ -1,6 +1,6 @@
 use sea_orm_migration::prelude::*;
 
-#[async_std::main]
+#[tokio::main]
 async fn main() {
-    cli::run_cli(pixles_api_migration::Migrator).await;
+    cli::run_cli(migration::Migrator).await;
 }
