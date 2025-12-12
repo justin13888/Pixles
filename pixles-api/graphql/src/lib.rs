7faaed03bd60b8d4a67a2c11154744ce52247d8d
Justin Chung <20733699+justin13888@users.noreply.github.com>
2025-12-11 22:30:21 -0500

    Add back GET handling for graphql

--- pixles-api/graphql/src/lib.rs
+++ pixles-api/graphql/src/lib.rs
@@ -107,7 +107,7 @@ pub async fn get_router<C: Into<GraphqlServerConfig>>(
     };
 
     // Build router
-    let mut app = Router::new().route("/", post(graphql_handler).with_state(state));
+    let mut app = Router::new().route("/", get(graphql_handler).post(graphql_handler).with_state(state));
     #[cfg(debug_assertions)]
     {
         app = app.route("/playground", get(|| graphiql("/v1/graphql")));
